ad824ad8bdb3113da6b116ed9ac6666d17fc1471
Steven Ly <steven.ly10213@gmail.com>
2021-03-30 17:36:55 -0600

    updated js and db.json

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const dawgURL = " http://localhost:3000/dawgs";
 const dogSection = document.querySelector(".cards");
+const dogContainer = document.querySelector(".card-container");
 
 fetch(dawgURL)
   .then((response) => response.json())
-  .then((dawgURL) => {
-    const li = document.createElement("li");
-    li.src = dawgURL.image;
-    dogSection.append(li);
+  .then((dawgArray) => {
+    dawgArray.forEach((dawgImg) => {
+      const li = document.createElement("img");
+      li.src = dawgImg.image;
+      dogSection.append(li);
+    });
   });
