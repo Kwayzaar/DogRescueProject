1f3fc9fe943333d5d2aba0a488155edf5cc5ee49
Steven Ly <steven.ly10213@gmail.com>
2021-03-30 14:13:42 -0600

    new db.json

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const baseURL = "https://dog.ceo/api/breed/hound/images";
 
 fetch(baseURL)
   .then((response) => response.json())
-  .then(dogData);
+  .then((dogData) => {
+    const dogCard = dogData.message[];
+    console.log(dogCard);
+  });
