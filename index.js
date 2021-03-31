8dae44fda487b1c81a6058c32476c519403f6808
Steven Ly <steven.ly10213@gmail.com>
2021-03-30 19:20:12 -0600

    little steps

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const dawgURL = " http://localhost:3000/dawgs";
-const dogSection = document.querySelector(".cards");
-const dogContainer = document.querySelector(".card-container");
+const dawgSection = document.querySelector(".cards");
+const dawgContainer = document.querySelector(".card-container");
 
 fetch(dawgURL)
   .then((response) => response.json())
   .then((dawgArray) => {
-    dawgArray.forEach((dawgImg) => {
-      const li = document.createElement("img");
-      li.src = dawgImg.image;
-      dogSection.append(li);
+    dawgArray.forEach((dawgs) => {
+      //getting images on the page
+      const dawgCard = document.createElement("div");
+
+      const dawgName = document.createElement("h2");
+      dawgName.textContent = dawgs.name;
+
+      const dawgImg = document.createElement("img");
+      dawgImg.src = dawgs.image;
+
+      const dawgBreed = document.createElement("p");
+      dawgBreed.textContent = dawgs.breed;
+
+      const dawgSex = document.createElement("p");
+      dawgSex.textContent = dawgs.sex;
+
+      const dawgAge = document.createElement("p");
+      dawgAge.textContent = dawgs.age;
+
+      dawgCard.append(dawgName, dawgImg, dawgBreed, dawgAge, dawgSex);
+      dawgContainer.append(dawgCard);
     });
   });
